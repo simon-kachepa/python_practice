d37b816f4c35046186ead6bde31b92406e57562e
Simon Kachepa <skachepa@devdg.digitalgeeksz.pl>
2023-12-16 18:06:12 +0200

    Update variables02.py

--- variables02.py
+++ variables02.py
@@ -8,4 +8,6 @@ print("The sum of 10 & 20 is: "+ str(10+20))
 print("The sum of num1 & num2 is: "+ sum)
 
 nationality = "Zimbabweans"
+code = 263
 print("Hello,", nationality)
+print("Your age is:", code)
