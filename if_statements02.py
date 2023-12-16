9d1c349d9a18059d98d4efdec639c8367fbe677b
Simon Kachepa <skachepa@devdg.digitalgeeksz.pl>
2023-12-16 20:44:04 +0200

    Create if_statements02.py

---
+++ if_statements02.py
@@ -0,0 +1,10 @@
+#Else if statements in Python
+
+age = int(input("Enter your age: "))
+
+if age < 18:
+    print("You are a child")
+elif age < 50:
+    print("You are an adult")
+else:
+    print("You are an elder")
