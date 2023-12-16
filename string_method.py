2fb47dc6f8c83f5b90bb182fdacce307533d6de2
Simon Kachepa <skachepa@devdg.digitalgeeksz.pl>
2023-12-16 18:07:14 +0200

    Create string_method.py

---
+++ string_method.py
@@ -0,0 +1,14 @@
+name = "Simon Kachepa"
+
+print(len(name)) # Prints the length of the str
+print(name.find("m")) # To find a character in a string. It returns the index of that character if the character exist, else it returns -1
+print(name.capitalize()) # Capitalize first letter of the str
+print(name.upper()) # Capitalize all letters of the str
+print(name.lower()) # Put all letters of the str in lowercase
+print(name.isalpha()) #Checks if the str contains alphabet letters only
+print(name.isdigit()) #Checks if the str contains digits only
+print(name.count("a")) #Count the occurance of a character
+print(name.replace("e", "a")) #Replaces a character in str
+print(name*3) #Prints the variables n times where n in this case is 3
+print(3*'+')
+print('+'* 3) 
