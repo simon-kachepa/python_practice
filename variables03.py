b19894a2b5b0b3ee77fd2a8a28bfb35ac966d716
Simon Kachepa <skachepa@devdg.digitalgeeksz.pl>
2023-12-16 18:08:16 +0200

    Create  variables03.py

---
+++ variables03.py
@@ -0,0 +1,23 @@
+# 1. string (str)
+#first_name = "Simon"
+#last_name = "Kachepa"
+#full_name = first_name + last_name
+#print("Hello", full_name)
+#print(type(full_name))
+
+# 2. integer (int)
+age = 10
+#print(age)
+#print(type(age))
+print("You are "+ str(age) + " years old")
+
+# 3. float
+#weight = 65.5
+#print(weight)
+#print(type(weight))
+#print("You are "+ weight, "kgs")
+
+# 4. boolean (bool)
+isMale = True
+print(isMale)
+print("Are you a boy? "+ str(isMale))
