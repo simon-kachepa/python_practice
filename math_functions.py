837cf49984e69711198aa15970aae611fdb39d92
Simon Kachepa <skachepa@devdg.digitalgeeksz.pl>
2023-12-16 20:45:12 +0200

    Create math_functions.py

---
+++ math_functions.py
@@ -0,0 +1,14 @@
+import math
+
+PI = 3.14
+x = 67
+y = 90
+z = 23
+
+#print(round(PI))
+#print(math.ceil(PI))
+#print(math.floor(PI))
+#print(abs(PI))
+#print(math.sqrt(PI))
+#print(max(x,y,z))
+#print(min(x,y,z))
