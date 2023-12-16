7a9d45563e8ee5f667f916d2cada5c079cfedae7
Simon Kachepa <skachepa@devdg.digitalgeeksz.pl>
2023-12-16 18:06:44 +0200

    Create assignment.py

---
+++ assignment.py
@@ -0,0 +1,18 @@
+#Assigning a value to 1 variable
+location = "Zimbabwe"
+
+# Assigning different values to different variables in 1 line (multiple assignment)
+first_name, last_name, age, email = "Simon", "Kachepa", 21, "kachepasimon@gmail.com"
+
+print(first_name)
+print(last_name)
+print(age)
+print(email)
+
+# Assigning same value to different variables
+x = y = z = 10
+
+print(x)
+print(y)
+print(z)
+
