e905e5eef31368425599c201f75c8e949b43ec05
Simon Kachepa <skachepa@devdg.digitalgeeksz.pl>
2023-12-16 20:42:13 +0200

    Update user_input.py

--- user_input.py
+++ user_input.py
@@ -1,2 +1,10 @@
+#Accepting str input
 name = input("Enter your name: ")
+age = int(input("Enter your age: "))
+height = float(input("Enter your height: "))
+
+age += 1
+
 print("Your name is: "+name)
+print("You are turning "+str(age)+" years old")
+print("You are "+ str(height)+" cm")
