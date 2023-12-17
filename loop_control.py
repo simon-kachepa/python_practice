7beaa752fa8b92ee250a8712795e93b250e69b71
Simon Kachepa <skachepa@devdg.digitalgeeksz.pl>
2023-12-17 07:21:31 +0200

    Create loop_control.py

---
+++ loop_control.py
@@ -0,0 +1,20 @@
+#loop control statements - change the execution of a loop from its normal way
+
+#break - stop and exits the loop.
+#continue - skips a certain part from executing
+#pass - does nothing
+
+#for i in range(10):
+#    if i == 5:
+#        break
+#    print(i)
+
+#for i in range(10):
+#    if i == 5:
+#        continue
+#    print(i)
+
+for i in range (10):
+    if i == 5:
+        pass
+    print(i)
