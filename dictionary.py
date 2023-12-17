64c16d615101f1085e0e1fae89229d5aafe3cae7
Simon Kachepa <skachepa@devdg.digitalgeeksz.pl>
2023-12-17 19:17:34 +0200

    Create  dictionary.py

---
+++ dictionary.py
@@ -0,0 +1,21 @@
+# dictionary - Is a changeable, unordered collection of value:key pairs
+
+capitals = {
+    "Zimbabwe": "Harare",
+    "SA": "Cape town",
+    "Mozambique": "Maputo",
+    "Zambia": "Lusaka"
+}
+
+#for key, value in capitals.items():
+#    print(key, "->", value)
+
+capitals.update({
+    "Botswana": "Gaboroni"
+})
+
+#print(capitals.keys())
+#print(capitals.values())
+#print(capitals.clear())
+print(capitals.get("Zimbabwe"))
+
