0906a7730352f8fe5fe05a2345212a241fda3c0a
Simon Kachepa <skachepa@devdg.digitalgeeksz.pl>
2023-12-17 19:15:45 +0200

    Create multi_dimensional_list.py

---
+++ multi_dimensional_list.py
@@ -0,0 +1,20 @@
+#2D List- lists of list
+
+wild_animals = ["lion", "elephant", "cheetar"]
+domestic_animals = ["cattle", "horses", "goats", "sheep"]
+aquatic_animals = ["fish", "crocodile", "hippo"]
+
+animals = [wild_animals, domestic_animals, aquatic_animals]
+
+#print(animals)
+#print(animals[0])
+#print(animals[1])
+#print(animals[2])
+#print(animals[0][0])
+#print(animals[0][1])
+#print(animals[1][0])
+#print(animals[1][1])
+#print(animals[1][2])
+# print(animals[1][3])
+# print(animals[2][0])
+print(animals[2][1])
