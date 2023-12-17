80295851214a39bdc88599f33639d9aa96fbc93e
Simon Kachepa <skachepa@devdg.digitalgeeksz.pl>
2023-12-17 19:16:23 +0200

    Create  tuples.py

---
+++ tuples.py
@@ -0,0 +1,12 @@
+#†uple - an ordered and unchangeable list
+
+student = ("Zviko", 16, "male")
+
+#for item in student:
+    # print(item)
+
+# print(student.count("Zviko"))
+# print(student.index("male"))
+
+if 16 in student:
+    print("Yes, we got your age Zviko")
