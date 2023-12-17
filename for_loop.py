a9b7492ccc2fd37f7d4b7b2367ef41d1568c8b1c
Simon Kachepa <skachepa@devdg.digitalgeeksz.pl>
2023-12-17 07:18:39 +0200

    Create for_loop.py

---
+++ for_loop.py
@@ -0,0 +1,10 @@
+# A for loop prints a block of code for a limited number of times]
+
+#for i in range(10):
+#   print(i)
+
+#for i in range(50, 100):
+#   print(i)
+
+for i in range(50, 100, 2):
+    print(i)
