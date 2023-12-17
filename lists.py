b7d2091d9c43d10ae268588c6ca01d9bc10d70a4
Simon Kachepa <skachepa@devdg.digitalgeeksz.pl>
2023-12-17 19:15:10 +0200

    Create lists.py

---
+++ lists.py
@@ -0,0 +1,24 @@
+#lists - used to store multiple values in a single variable
+
+fruits = ["mangoe", "orange", "banana", "lemon"]
+
+#print(fruits)
+#print(fruits[0])
+#print(fruits[1])
+#print(fruits[2])
+#print(fruits[3])
+#for fruit in fruits:
+#    print(fruit)
+
+#fruits[0] = "guava"
+#print(fruits[0])
+
+#fruits.append("nutchis")
+#fruits.insert(2, "Pawpaw")
+#fruits.pop()
+#fruits.remove("orange")
+#fruits.reverse()
+#fruits.sort()
+
+for fruit in fruits:
+    print(fruit)
