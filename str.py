91a8f092d1f579f2676abc898fbe2de081e8546c
Simon Kachepa <skachepa@devdg.digitalgeeksz.pl>
2023-12-18 16:18:16 +0200

    Create  str.py

---
+++ str.py
@@ -0,0 +1,18 @@
+# str = "Holberton School"
+# print(str*3)
+# print(str[:9])
+
+# str1 = "Holberton"
+# str2 = "School"
+# str1 = str1 + " " + str2
+
+# print(f"Welcome to {str1}!")
+
+# word = "Holberton"
+
+# first_3_word, last_2_word, word_middle = word[:3], word[-2:], word[1:-1]
+# print(first_3_word)
+# print(last_2_word)
+# print(word_middle)
+
+import this
