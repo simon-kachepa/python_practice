8d592391d52f504e90174c27ad24bd9e8dcbd7c1
Simon Kachepa <skachepa@devdg.digitalgeeksz.pl>
2023-12-18 16:18:50 +0200

    Create cs50-qn1.py

---
+++ cs50-qn1.py
@@ -0,0 +1,5 @@
+answer = input("What is the Answer to the Greate Question of Life, Universe and Everything? ")
+if answer == "42" or answer.lower() == "forty-two" or answer.lower() == "forty two":
+    print("Yes")
+else:
+    print("No")
