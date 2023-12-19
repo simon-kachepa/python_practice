75df4a8287f4b19d2fbc6bca994b8454780b728d
Simon Kachepa <skachepa@devdg.digitalgeeksz.pl>
2023-12-19 20:51:44 +0200

    Create write_file.py

---
+++ write_file.py
@@ -0,0 +1,7 @@
+# with open("text.txt", 'w') as file:
+#     file.write("Hello better tomorrow!")
+
+text = "Working tirelesly for a better tomorrow"
+
+with open("test.txt", 'w') as file:
+    file.write(text)
