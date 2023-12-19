018a96c4962f198303f842d21954c9a4c030d147
Simon Kachepa <skachepa@devdg.digitalgeeksz.pl>
2023-12-19 12:14:17 +0200

    Create str_format02.py

---
+++ str_format02.py
@@ -0,0 +1,14 @@
+num1 = 12.865
+
+# print("The first number is: {}".format(num1))
+# print("The number to 2 sf is: {:.2f}".format(num1))
+# print("The number to 5 sf is: {:.5f}".format(num1))
+
+num2 = 1000
+print("My number with comma at thousand position is: {:,}".format(num2))
+print("My number in binary is: {:b}".format(num2))
+print("My number in octal is: {:o}".format(num2))
+print("My number in hex is: {:x}".format(num2))
+print("My number in HEX is: {:X}".format(num2))
+print("My number in scientific notation is: {:e}".format(num2))
+print("My number in SCIENTIFIC NOTATION is: {:E}".format(num2))
