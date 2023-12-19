51ada154275aff4c231412f4ed9a322d32eec6a2
Simon Kachepa <skachepa@devdg.digitalgeeksz.pl>
2023-12-19 12:13:45 +0200

    Create str_format01.py

---
+++ str_format01.py
@@ -0,0 +1,7 @@
+name = "Simon"
+
+# print("Hello {}. You have something great waiting for you.".format(name))
+#print("Hello {:10}. You have something great waiting for you ahead!".format(name))
+print("Hello {:>10}. You have something great waiting for you ahead!".format(name))
+print("Hello {:>10}. You have something great waiting for you ahead!".format(name))
+print("Hello {:^10}. You have something great waiting for you ahead!".format(name))
