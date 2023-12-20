0c373ec316e74b77bc6dba27f5d721e1e348f076
Simon Kachepa <skachepa@devdg.digitalgeeksz.pl>
2023-12-20 14:21:11 +0200

    Create  octal_operations.py

---
+++ octal_operations.py
@@ -0,0 +1,15 @@
+oct1 = 0o10
+oct2 = 0o11
+
+print(oct1 + oct2)
+print(oct(oct1 + oct2))
+print(oct(oct1 & oct2))
+print(oct(oct1 | oct2))
+
+oct3 = 0O10
+oct4 = 0O11
+
+print(oct3 + oct4)
+print(oct(oct3 + oct4))
+print(oct(oct3 & oct4))
+print(oct(oct3 | oct4))
