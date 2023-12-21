c0ea6d058b93f23c63e732526d3cf929fb9fda6a
Simon Kachepa <skachepa@devdg.digitalgeeksz.pl>
2023-12-21 15:55:20 +0200

    Create lists00.py

---
+++ lists00.py
@@ -0,0 +1,15 @@
+# a list -> is a collection of ordered, indexed and changeable items.
+
+brothers_list = ["Henry", "John", "Simon", "Tichaona", "Munyaradzi", "Taurai"]
+
+# print(brothers_list)
+# print(brothers_list[0])
+# print(brothers_list[1])
+# print(brothers_list[0:6])
+# print(brothers_list[:6])
+# print(brothers_list[3:len(brothers_list)])
+
+brothers_list[0] = "Newten"
+
+print(brothers_list)
+print(brothers_list[::-1])
