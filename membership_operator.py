980f22c2129b6a7a3451c9f1bad837735b03d7fc
Simon Kachepa <skachepa@devdg.digitalgeeksz.pl>
2023-12-22 21:41:56 +0200

    Create membership_operator.py

---
+++ membership_operator.py
@@ -0,0 +1,14 @@
+name = "Kachepa"
+
+# The in operator
+# print("Kach" in name)
+# print("h" in name)
+# print("m" in name)
+
+# The not in operator
+# print("Kach" not in name)
+# print("c" not in name)
+# print("m" not in name)
+
+# The operators without a variable
+print("mo" in "Simon")
