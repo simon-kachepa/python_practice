462d27cd7b383f9497c11ddf82e6ffc6dfddc82e
Simon Kachepa <skachepa@devdg.digitalgeeksz.pl>
2023-12-22 21:42:21 +0200

    Create  str_slicing.py

---
+++ str_slicing.py
@@ -0,0 +1,14 @@
+str = "Kuchemedza mombe ndokuneyi"
+
+# print(str)
+# print(str[0:26])
+# print(str[:26])
+# print(str[::])
+# print(str[:15])
+# print(str[2:26:3])
+# print(str[::2])
+
+#printing the str in reverse
+# print(str[::-1])
+# print(str[-1::-1])
+print(str[-1:-8:-1])
