3ba159512080463b61de8f7d9e17d2520b660dd5
Simon Kachepa <skachepa@devdg.digitalgeeksz.pl>
2023-12-23 23:57:26 +0200

    Update  circumference.py

--- circumference.py
+++ circumference.py
@@ -3,4 +3,4 @@ import math
 pi = math.pi
 radius = float(input("Enter the radius: "))
 circumference = 2 * pi * radius
-print(f"The circumference is: {circumference}")
+print(f"The circumference is: {circumference:.2f}")
