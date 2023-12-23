2385e50bc5475b790edb1f5f2a63127b608bd921
Simon Kachepa <skachepa@devdg.digitalgeeksz.pl>
2023-12-23 22:13:34 +0200

    Create f_string01.py

---
+++ f_string01.py
@@ -0,0 +1,6 @@
+num1 = 40
+num2 = 35
+
+# print(f"The value of num1 + num2 is: {num1 + num2}")
+print(f"The value of {{num1 + num2}} is: {num1 + num2}")
+print(f"The value of {{num1 + num2}} is: {{{num1 + num2}}}")
