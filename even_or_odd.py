e88edc6ca79c8bdedb1364ca45d40a4559ad4e28
Simon Kachepa <skachepa@devdg.digitalgeeksz.pl>
2023-12-23 23:30:04 +0200

    Create even_or_odd.py

---
+++ even_or_odd.py
@@ -0,0 +1,8 @@
+# Problem: Write a program to check whether the given number is even or not.
+
+number = int(input("Enter a number: "))
+
+if number % 2 == 0:
+    print(f"{number} is even")
+else:
+    print(f"{number} is odd")
