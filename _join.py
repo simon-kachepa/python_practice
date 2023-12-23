8771e24fd9178da8849cb926b360649c3a56cd0f
Simon Kachepa <skachepa@devdg.digitalgeeksz.pl>
2023-12-23 23:29:14 +0200

    Create _join.py

---
+++ _join.py
@@ -0,0 +1,12 @@
+# syntax -> seperator.join(iterable)
+
+list1 = ['S', 'i', 'm', 'o', 'n']
+# print(''.join(list1))
+# print(' '.join(list1))
+# print('_'.join(list1))
+
+list2 = ["My", "name", "is", "Simon"]
+# print(" ".join(list2))
+
+dictionary_01 = {"name": "Simon", "age":13}
+print(" and ".join(dictionary_01))
