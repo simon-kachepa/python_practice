91118703d5b0c60e57a7a5cd1eefd94360fabc8a
Simon Kachepa <skachepa@devdg.digitalgeeksz.pl>
2023-12-23 23:29:35 +0200

    Create _replace.py

---
+++ _replace.py
@@ -0,0 +1,5 @@
+# syntax -> string.replace(old_string, new_string, count)
+str = "My favourite name is Simon"
+print(str.replace("Simon", "Hardazy"))
+print(str.replace(" ", "-", 2))
+print(str.replace(" ", "-"))
