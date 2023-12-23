8c21a701566a9432fc39331cae96d64fcac579c8
Simon Kachepa <skachepa@devdg.digitalgeeksz.pl>
2023-12-23 22:14:10 +0200

    Create _strip.py

---
+++ _strip.py
@@ -0,0 +1,11 @@
+name = "             Simon           "
+print(name.strip())
+
+last_name = "##########Kachepa########"
+print(last_name.strip('#'))
+
+nick_name = " ####Hardazy####"
+print(nick_name.strip('#'))
+
+full_name = "Simon Kachepa"
+print(full_name.strip("Spian"))
