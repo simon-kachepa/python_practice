e809175f8a3bd406615e49c00eb22e0870e1404c
Simon Kachepa <skachepa@devdg.digitalgeeksz.pl>
2023-12-23 23:30:34 +0200

    Create degree_to_fahrenheit.py

---
+++ degree_to_fahrenheit.py
@@ -0,0 +1,3 @@
+temp_degrees = float(input("Enter the temperature in degrees Celcious: "))
+temp_fehrenheit = (temp_degrees * (9/5)) + 32
+print(f"The temperature in Fehrenheit is: {temp_fehrenheit}")
