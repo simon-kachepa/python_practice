42d0859490dd06dac641dc8bc42aa797ee8a18ed
Simon Kachepa <skachepa@devdg.digitalgeeksz.pl>
2023-12-24 14:17:16 +0200

    Create _isupper.py

---
+++ _isupper.py
@@ -0,0 +1,11 @@
+# first_name = "SIMON"
+# if first_name.isupper():
+#     print("Your name is in upper case")
+# else:
+#     print("Your name is in lower case")
+
+first_name = "Simon"
+if first_name.isupper():
+    print("Your name is in upper case")
+else:
+    print("Your name is in lower case")
