0a7b538b45f2a4e2fde8c13d17ec207218d1774c
Simon Kachepa <skachepa@devdg.digitalgeeksz.pl>
2023-12-24 14:16:51 +0200

    Create  _find.py

---
+++ _find.py
@@ -0,0 +1,11 @@
+str = "My real name is cool, but i like my nickname"
+
+# print(str.find("b"))
+# print(str.find("i"))
+# print(str.find("name"))
+# print(str.find("h"))
+
+# print(str.find("l", 0, 7))
+# print(str.find("b", 1, 10))
+# print(str.find("i", 21, 30))
+print(str.find("b", 1, 11))
