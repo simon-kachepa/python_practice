2e1fc532a962731242f0aa8431328b6fe4da799a
Simon Kachepa <skachepa@devdg.digitalgeeksz.pl>
2023-12-24 19:07:11 +0200

    Create  question02.py

---
+++ question02.py
@@ -0,0 +1,4 @@
+# Question: Write a program to print every character of a string entered by user in a new line using loop.
+str = input("Enter the string: ")
+for character in str:
+    print(character)
