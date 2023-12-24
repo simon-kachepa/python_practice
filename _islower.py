bd5db41ab8a80381b0b6ef139009c2d23f0cdd53
Simon Kachepa <skachepa@devdg.digitalgeeksz.pl>
2023-12-24 14:18:03 +0200

    Create _islower.py

---
+++ _islower.py
@@ -0,0 +1,11 @@
+# first_name = "simon"
+# if first_name.islower():
+#     print("Your name is in lower case")
+# else:
+#     print("Your name is in upper case")
+
+first_name = "Simon"
+if first_name.islower():
+    print("Your name is in lower case")
+else:
+    print("Your name has some upper cases")
