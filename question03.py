284e5574c099f2cc232d3e82e4472edb13f136d9
Simon Kachepa <skachepa@devdg.digitalgeeksz.pl>
2023-12-24 19:07:44 +0200

    Create question03.py

---
+++ question03.py
@@ -0,0 +1,6 @@
+# Question: Write a program to find the length of the string "refrigerator" without using len function.
+str = "refrigerator"
+length = 0
+for character in str:
+    length += 1
+print(length)
