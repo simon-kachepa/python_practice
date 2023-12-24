7732cf92a37600f591b4a0cae0ce4d2f8fe7875f
Simon Kachepa <skachepa@devdg.digitalgeeksz.pl>
2023-12-24 19:08:46 +0200

    Create question05.py

---
+++ question05.py
@@ -0,0 +1,6 @@
+#Question: Write a program to check if the word 'orange' is present in the "This is orange juice".
+str = "This is orange juice"
+if ("orange" in str):
+    print("orange found")
+else:
+    print("orange not found")
