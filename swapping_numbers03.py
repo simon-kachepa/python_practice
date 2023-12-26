ecff227a8c69d8d9017c8089afe12c6a3508a65b
Simon Kachepa <skachepa@devdg.digitalgeeksz.pl>
2023-12-26 22:36:13 +0200

    Create swapping_numbers03.py

---
+++ swapping_numbers03.py
@@ -0,0 +1,5 @@
+num1 = int(input("Enter the first number: "))
+num2 = int(input("Enter the second number: "))
+print(f"Originally -> num1 = {num1}, num2 = {num2}")
+num1, num2 = num2, num1
+print(f"Swapped -> num1 = {num1}, num2 = {num2}")
