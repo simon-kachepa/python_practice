84e743d9bcf37fe790f479444f082a6646888d4d
Simon Kachepa <skachepa@devdg.digitalgeeksz.pl>
2023-12-27 21:18:45 +0200

    Create  list_comprehension.py

---
+++ list_comprehension.py
@@ -0,0 +1,6 @@
+names_list = ["Simon", "Simeon", "Captain", "Zviko", "Synclaire", "Tino", "Samuel"]
+names_sublist = []
+for name in names_list:
+    if name[0] == 'S':
+        names_sublist.append(name)
+print(names_sublist)
