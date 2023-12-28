eb00335117199f309245d213108bf2772b921fb1
Simon Kachepa <skachepa@devdg.digitalgeeksz.pl>
2023-12-28 20:37:55 +0200

    Create  unique_list.py

---
+++ unique_list.py
@@ -0,0 +1,16 @@
+def unique_list(list):
+    traversed_list = []
+    deleted = []
+    for item in list:
+        if item in deleted or item in traversed_list:
+            deleted.append(item)
+            traversed_list.remove(item)
+        else:
+            traversed_list.append(item)
+    return traversed_list
+def main():
+    list1 = [1, 2, 3, 2, 4, 1, 5]
+    result = unique_list(list1)
+    print(result)
+
+main()
