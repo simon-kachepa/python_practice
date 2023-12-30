8dc1fa25def72cf0f7097e2aad23c107b17b0e2e
Simon Kachepa <skachepa@devdg.digitalgeeksz.pl>
2023-12-30 21:29:11 +0200

    Create  for_loop_else.py

---
+++ for_loop_else.py
@@ -0,0 +1,7 @@
+favourite_language = ["Python", "C", "C++", "Javascript"]
+for language in favourite_language:
+    if language == "C":
+        print(f"I like C")
+        break
+else:
+    print(f"I don't like C")
