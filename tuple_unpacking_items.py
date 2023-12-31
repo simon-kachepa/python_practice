f545e3e36f48ce74c8fe52249a7509f2c6a5e737
Simon Kachepa <skachepa@devdg.digitalgeeksz.pl>
2023-12-31 21:19:58 +0200

    Create tuple_unpacking_items.py

--- tuple_unpacking_items.py
+++ tuple_unpacking_items.py
@@ -1,5 +1,6 @@
-animals = ("lions", "elephants", "cheetars", "ducks")
-animal1, animal2, animal3, animal4 = animals
+# Tuple unpacking
+animals = ("lions", "elephants", "cheetars", "ducks", "peacock")
+animal1, animal2, animal3, animal4, animal5 = animals
 print(animal1)
 print(animal2)
 print(animal3)
