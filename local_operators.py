0f3b5893fce50e42c2fcfb7563cf0d17af12e24c
Simon Kachepa <skachepa@devdg.digitalgeeksz.pl>
2023-12-31 21:15:42 +0200

    Create local_operators.py

--- local_operators.py
+++ local_operators.py
@@ -4,5 +4,5 @@ age = int(input("Enter your age: "))
 
 if age >= 18 and age <= 50:
     print("You are a good fit for the job, you can work")
-elif age < 18 or age > 50:
+else:
     print("Your age does not allow you to work")
