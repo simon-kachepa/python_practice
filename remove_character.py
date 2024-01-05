110f776de6b459fda00248d390e014c72e333705
Simon Kachepa <skachepa@devdg.digitalgeeksz.pl>
2024-01-05 19:57:08 +0200

    Create remove_character.py

---
+++ remove_character.py
@@ -0,0 +1,15 @@
+def remove_char_at(str, n):
+    if n < 0:
+        return str
+    else:
+        new_str = str[:n] + str[n+1:]
+        return new_str
+
+def main():
+    print(remove_char_at("Best School", 3))
+    print(remove_char_at("Chicago", 2))
+    print(remove_char_at("C is fun!", 0))
+    print(remove_char_at("School", 10))
+    print(remove_char_at("Python", -2))
+
+main()
