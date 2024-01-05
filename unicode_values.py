67d2be590f7ba811d7020af188716dd5c9f82dac
Simon Kachepa <skachepa@devdg.digitalgeeksz.pl>
2024-01-05 19:56:49 +0200

    Create  unicode_values.py

---
+++ unicode_values.py
@@ -0,0 +1,9 @@
+# print(ord("a"))
+
+# Printing unicode value and their respective charactersthey represent
+# for i in range(ord('a'), ord('z') + 1):
+#     print("{} -> {}".format(i, chr(i)))
+
+#Printing unicode characters
+for c in range(97, 123):
+    print(chr(c))
