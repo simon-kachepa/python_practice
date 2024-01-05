e0f809b20b60fef62b6206d6bbb33347712a5d80
Simon Kachepa <skachepa@devdg.digitalgeeksz.pl>
2024-01-05 19:57:28 +0200

    Create list_copying.py

---
+++ list_copying.py
@@ -0,0 +1,17 @@
+def new_in_list(my_list, idx, element):
+    if idx < 0 or idx >= len(my_list):
+        return my_list
+    else:
+        new_list = my_list[:]
+        new_list[idx] = element
+        return new_list
+def main():
+    my_list = [1, 2, 3, 4, 5]
+    idx = 3
+    new_element = 9
+    new_list = new_in_list(my_list, idx, new_element)
+
+    print(new_list)
+    print(my_list)
+
+main()
