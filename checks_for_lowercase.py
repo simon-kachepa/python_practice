69774a54be9b0f2bedd72d053db056a781d631d3
Simon Kachepa <skachepa@devdg.digitalgeeksz.pl>
2024-01-05 19:55:29 +0200

    Create checks_for_lowercase.py

---
+++ checks_for_lowercase.py
@@ -0,0 +1,13 @@
+def islower(c):
+    if ord(c) >= 97 and ord(c) <= 122:
+        return True
+    else:
+        return False
+    
+def main():
+    print("a is {}".format("lower" if islower("a") else "upper"))
+    print("H is {}".format("lower" if islower("H") else "upper"))
+    print("A is {}".format("lower" if islower("A") else "upper"))
+    print("3 is {}".format("lower" if islower("3") else "upper"))
+    print("g is {}".format("lower" if islower("g") else "upper")) 
+main()
