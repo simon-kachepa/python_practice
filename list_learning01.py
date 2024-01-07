637e278e5233fae117a5e5311b658a43a4a2de43
Simon Kachepa <skachepa@devdg.digitalgeeksz.pl>
2024-01-07 21:33:50 +0200

    Create  list_learning01.py

---
+++ list_learning01.py
@@ -0,0 +1,16 @@
+list_1 = [12, 67, 89, 65, 45, 90, 63, 21, 12]
+list_2 = [43, 61, 12, 89, 43, 67, 9, 17, 6, 81]
+list_3 = [54, 26, 93, 6, 39, 72, 40, 89, 100]
+
+# Sorting elements of list_1 using sort method
+list_1.sort()
+print(list_1)
+
+# Sorting list_2 in reverse
+list_2.sort(reverse=True)
+print(list_2)
+
+# Reverse the elements of list_3
+list_3.reverse()
+print(list_3)
+
