ef77e225a8c57075c2e17545c0f3ba30ef5bf1ff
Simon Kachepa <skachepa@devdg.digitalgeeksz.pl>
2024-01-09 22:45:50 +0200

    Create d_linked_list03.c

--- C_codes/Doubly linked list/d_linked_list03.c
+++ C_codes/Doubly linked list/d_linked_list03.c
@@ -11,6 +11,7 @@ typedef struct Node
 Node *add_to_empty(Node *head, int data);
 Node *add_at_begginning(Node *head, int data);
 void inserting_at_end(Node *head, int data);
+void add_at_certain_position(Node *head, int position, int data);
 
 int main(void)
 {
@@ -18,6 +19,8 @@ int main(void)
     head = add_to_empty(head, 200);
     head = add_at_begginning(head, 100);
     inserting_at_end(head, 300);
+    add_at_certain_position(head, 2, 800);
+    add_at_certain_position(head, 5, 600);
 
     while (head)
     {
@@ -62,10 +65,42 @@ void inserting_at_end(Node *head, int data)
     tmp->next = NULL;
 
     ptr = head;
-    while(ptr)
+    while(ptr->next)
     {
         ptr = ptr->next;
     }
     ptr->next = tmp;
     tmp->prev = ptr;
+}
+
+void add_at_certain_position(Node *head, int position, int data)
+{
+    Node *current = head;
+    Node *next_n = NULL;
+    Node *tmp = NULL;
+
+    tmp = malloc(sizeof(Node));
+    tmp->next = NULL;
+    tmp->data = data;
+    tmp->prev = NULL;
+
+    position--;
+    while (position > 1)
+    {
+        current = current->next;
+        position--;
+    }
+    if (current->next == NULL)
+    {
+        current->next = tmp;
+        tmp->prev = current;
+    }
+    else
+    {
+        next_n = current->next;
+        current->next = tmp;
+        next_n->prev = tmp;
+        tmp->prev = current;
+        tmp->next = next_n;
+    }
 }
