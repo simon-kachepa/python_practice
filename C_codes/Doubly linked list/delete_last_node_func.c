f7f8f381439ab571684e5d7a198cb6c401dca962
Simon Kachepa <skachepa@devdg.digitalgeeksz.pl>
2024-01-09 23:55:10 +0200

    Create delete_last_node_func.c

---
+++ C_codes/Doubly linked list/delete_last_node_func.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+typedef struct Node
+{
+    struct Node *prev;
+    int data;
+    struct Node *next;
+}Node;
+
+void delete_last_node(Node *head)
+{
+    Node *tmp = head;
+    Node * current;
+
+    if (tmp)
+    {
+        while (tmp->next != NULL)
+        {
+            tmp = tmp->next;
+        }
+        current = tmp->prev;
+        current->next = NULL;
+        free(tmp);
+        tmp = NULL;
+    }
+    
+}
