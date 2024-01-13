fb491e4e03f4beda48a981aaacf11f299a36f51c
Simon Kachepa <skachepa@devdg.digitalgeeksz.pl>
2024-01-13 22:59:16 +0200

    Update stack00.c

--- C_codes/STACKS/stack00.c
+++ C_codes/STACKS/stack00.c
@@ -1,4 +1,5 @@
 #include <stdio.h>
+#include <stdlib.h>
 #define ARR_SIZE 4
 
 int stack_arr[ARR_SIZE];
@@ -6,15 +7,28 @@ int top = -1;
 
 void push(int data);
 void print_stack_data(void);
+int pop();
 
 int main(void)
 {
+    int data;
+    // Trying to print the stack elements before pushing elements
     print_stack_data();
+
+    // Pushing elements to the stack and printing the pushed elements
     push(100);
     push(200);
     push(300);
     push(400);
     print_stack_data();
+
+    // Removing elements from the stack and printing the stack data after pop
+    data = pop();
+    data = pop();
+    data = pop();
+    print_stack_data();
+
+
     return (0);
 }
 
@@ -40,7 +54,25 @@ void print_stack_data(void)
     }
     while (index <= top)
     {
-        printf("%d\n", stack_arr[index]);
+        if (index == top)
+            printf("%d\n", stack_arr[index]);
+        else
+            printf("%d, ", stack_arr[index]);
         index++;
     }
+}
+
+int pop()
+{
+    int element;
+
+    if (top == -1)
+    {
+        printf("Stack underflow\n");
+        exit(1);
+    }
+    element = stack_arr[top];
+    top -= 1;
+
+    return (element);
 }
