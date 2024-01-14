a99b2a15c89a79f95ae217fb4f8ec31c9eb94702
Simon Kachepa <skachepa@devdg.digitalgeeksz.pl>
2024-01-14 23:46:50 +0200

    Update  reversing_str_words.py

--- reversing_str_words.py
+++ reversing_str_words.py
@@ -1,7 +1,7 @@
 my_string = "Hardazy the greatest man. I like coding"
 my_string = my_string.split()
 for word in my_string[::-1]:
-    if word == my_string[1]:
+    if word == my_string[0]:
         print(word)
     else:
         print(word, end='..')
