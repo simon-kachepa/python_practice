9eb35bad96b548b9b6132a079cbd0eb402f7b6c7
Simon Kachepa <skachepa@devdg.digitalgeeksz.pl>
2024-01-21 22:58:13 +0200

    Create stack_linked_list01.c

---
+++ C_codes/STACKS/stack_linked_list01.c
@@ -0,0 +1,59 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+typedef struct Node
+{
+    int data;
+    struct Node *link;
+}Node;
+
+Node *top = NULL;
+
+void print(void);
+void push(int data);
+
+int main(void)
+{
+
+    push(20);
+    push(90);
+    push(67);
+    print();
+
+    return (0);
+}
+
+void print(void)
+{
+    Node *ptr;
+
+    ptr = top;
+    if (!ptr)
+    {
+        printf("Stack underflow\n");
+        exit(1);
+    }
+    printf("Stack Element(s): ");
+    while (ptr)
+    {
+        printf("%d ", ptr->data);
+        ptr = ptr->link;
+    }
+    printf("\n");
+}
+
+void push(int data)
+{
+    Node *new_node = malloc(sizeof(Node));
+
+    if (!new_node)
+    {
+        printf("Stack overflow\n");
+        exit(1);
+    }
+    new_node->data = data;
+    new_node->link = NULL;
+
+    new_node->link = top;
+    top = new_node;
+}
