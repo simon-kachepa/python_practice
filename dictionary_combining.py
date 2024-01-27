700729b372a6fa878a76f839b76e817073814c02
Simon Kachepa <skachepa@devdg.digitalgeeksz.pl>
2024-01-27 22:51:09 +0200

    Update dictionary_combining.py

--- dictionary_combining.py
+++ dictionary_combining.py
@@ -1,9 +1,12 @@
 def combining_dict(my_dict1, my_dict2):
     my_dict1.update(my_dict2)
     return my_dict1
+
 def main():
     team1 = {'John': 15, 'Lisa': 10, 'Mike': 12}
     team2 = {'Emily': 8, 'Chris': 20, 'Mike': 15}
     result = combining_dict(team1, team2)
     print(result)
-main()
+
+if __name__ == "__main__":
+    main()
