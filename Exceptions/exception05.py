dd26667783e0fa3da8b7a6069e85b2d7e758ee2b
Simon Kachepa <skachepa@devdg.digitalgeeksz.pl>
2024-01-31 17:55:35 +0200

    Create Exceptions/exception05.py

---
+++ Exceptions/exception05.py
@@ -0,0 +1,21 @@
+def main():
+    x = get_int()
+    print(f"x is {x}")
+
+
+
+
+def get_int():
+    while True:
+        try:
+            x = int(input("Enter a number: "))
+        
+        except ValueError:
+            print("x entered is not a number")
+        
+        else:
+            break
+    return x
+
+if __name__== "__main__":
+    main()
