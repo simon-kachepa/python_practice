a4f677c2cbfe63f43650affdaf6a93307a2ece34
Simon Kachepa <skachepa@devdg.digitalgeeksz.pl>
2024-01-31 18:00:05 +0200

    Create Exceptions/exception09.py

---
+++ Exceptions/exception09.py
@@ -0,0 +1,17 @@
+def main():
+    x = get_int("What's x: ")
+    print(f"x is {x}")
+
+
+
+
+def get_int(prompt):
+    while True:
+        try:
+            return int(input(prompt))
+        
+        except ValueError:
+            pass
+
+if __name__== "__main__":
+    main()
