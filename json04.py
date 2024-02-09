530ec53d1d4ad93ad3c322179d5a1891a9982a70
Simon Kachepa <skachepa@devdg.digitalgeeksz.pl>
2024-02-09 02:06:58 +0200

    Create json04.py

---
+++ json04.py
@@ -0,0 +1,30 @@
+import json
+
+##Reading contents from a json file
+
+# person_JSON = '{"name": "Simon", "surname":"Kachepa", "age":10}'
+
+# with open("person.json", mode="w", encoding="UTF-8") as json_file:
+#     json_file.write(person_JSON)
+#     print("Person saved successfully")
+
+with open("person.json", encoding="UTF-8") as json_file:
+    person_JSON = json_file.read()
+    print(person_JSON)
+    person = json.loads(person_JSON)
+    print(person)
+    name = person["name"]
+    surname = person["surname"]
+    age = person["age"]
+
+print(f"The person fetched from the JSON file is called {name} {surname} and is {age} years old")
+print(person)
+
+print("----------------------------------------------")
+
+with open("person.json", mode="w", encoding="UTF-8") as json_file:
+    person["name"] = "Hardazy"
+    new_person_JSON = json.dumps(person)
+    json_file.write(new_person_JSON)
+    print("Saved successfully")
+
