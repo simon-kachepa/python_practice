7db4f7fb498e79a9d5c7547f170f81827c2b0758
Simon Kachepa <skachepa@devdg.digitalgeeksz.pl>
2024-02-10 22:51:53 +0200

    Crate oop_02_00.py

---
+++ Python_classes/OOP-JOURNEY/oop_02_00.py
@@ -0,0 +1,19 @@
+##starting another series of learning object oriented programming to fully understand the concepts
+
+class Employee:
+    pass
+
+##creating instances of Employee
+employee1 = Employee()
+employee2 = Employee()
+
+print(employee1)
+print(employee2)
+
+##creating instance variables (variables of the instances)
+employee1.name = "Simon"
+employee2.name = "Hardazy"
+
+##printing instance variables
+print(employee1.name)
+print(employee2.name)
