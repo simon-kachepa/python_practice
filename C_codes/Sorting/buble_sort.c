37002247a3ece1e321c7fad922f278b3338dac17
Simon Kachepa <skachepa@devdg.digitalgeeksz.pl>
2024-02-19 23:09:31 +0200

    Create buble_sort.c

---
+++ C_codes/Sorting/buble_sort.c
@@ -0,0 +1,41 @@
+#include <stdio.h>
+
+void bubble_sort(int arr[], int n);
+void printArray(int arr[], int size);
+int main(void)
+{
+    int my_arr[] = {90, 7, 8, 24, 1, 80};
+    int arr_size = sizeof(my_arr)/ sizeof(my_arr[0]);
+
+    bubble_sort(my_arr, arr_size);
+    printf("Sorted array: ");
+    printArray(my_arr, arr_size);
+    return 0;
+}
+
+void bubble_sort(int *arr, int n)
+{
+    int sorted_elements, index;
+
+    for (sorted_elements = 0; sorted_elements < n - 1; sorted_elements++)
+    {
+        for (index = 0; index < n - sorted_elements - 1; index++)
+        {
+            if (arr[index] > arr[index + 1])
+            {
+                int tmp = arr[index];
+                arr[index] = arr[index + 1];
+                arr[index + 1] = tmp;
+            }
+            printArray(arr, n);
+        }
+    }
+}
+
+void printArray(int arr[], int size) 
+{ 
+    int i; 
+    for (i = 0; i < size; i++) 
+        printf("%d ", arr[i]); 
+    printf("\n"); 
+}
