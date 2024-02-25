6b3b078824fb0704aa94185ca723e62860951437
Simon Kachepa <122989521+simon-kachepa@users.noreply.github.com>
2024-02-25 20:09:15 +0200

    Update class_square06.py

--- Python_classes/class_square06.py
+++ Python_classes/class_square06.py
@@ -22,8 +22,8 @@ class Square:
         if self.__size == 0:
             print()
         else:
-            for i in range(self.__size):
-                for j in range(self.__size):
+            for _ in range(self.__size):
+                for _ in range(self.__size):
                     print("#", end='')
                 print("")
 
