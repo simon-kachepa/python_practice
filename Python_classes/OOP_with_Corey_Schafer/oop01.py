8653596b83407151713c1473600dfb57b6b24c7e
Simon Kachepa <skachepa@devdg.digitalgeeksz.pl>
2024-03-03 20:49:40 +0200

    Create oop01.py

---
+++ Python_classes/OOP_with_Corey_Schafer/oop01.py
@@ -0,0 +1,13 @@
+## THis marks the beginning of learning OOP concepts with Corey Schafer
+## This programs gives introduction to the cfeation of classes
+
+class Employee:
+    pass
+
+employee1 = Employee()
+employee2 = Employee()
+
+print()
+print(employee1)
+print(employee2)
+print()
