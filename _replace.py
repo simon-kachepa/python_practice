52be6ff8f6221729c029169dc10a49c8afa44187
Simon Kachepa <122989521+simon-kachepa@users.noreply.github.com>
2024-03-22 19:18:37 +0200

    Update _replace.py

--- _replace.py
+++ _replace.py
@@ -1,6 +1,6 @@
 ## Program to find a piece of str and replace it
 # syntax -> string.replace(old_string, new_string, count)
 str = "My favourite name is Simon"
-print(str.replace("Simon", "Hardazy"))
+print(str.replace("Simon", "Hardazy"))#replaces the string 'Simon' in str
 print(str.replace(" ", "-", 2))
 print(str.replace(" ", "-"))
