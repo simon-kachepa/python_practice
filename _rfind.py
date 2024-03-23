f0637cefeec7db3ef36b15a5187d9d447a4e158e
Simon Kachepa <122989521+simon-kachepa@users.noreply.github.com>
2024-03-23 23:57:41 +0200

    Update _rfind.py

--- _rfind.py
+++ _rfind.py
@@ -7,5 +7,5 @@ str = "My real name is cool, but i like my nickname"
 
 # print(str.find("l", 0, 7))
 # print(str.find("b", 1, 10))
-# print(str.find("i", 21, 30))
+print(str.find("i", 21, 30))
 print(str.find("b", 1, 11))
